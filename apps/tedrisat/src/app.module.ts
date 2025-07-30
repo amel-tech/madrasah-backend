4bcebf27a892afb635e05f83ca6d6d8208226730
zahidcakici <cakici19@itu.edu.tr>
2025-07-30 22:36:19 +0300

    feat: implement drizzle orm with postgresql and example crud operations

--- apps/tedrisat/src/app.module.ts
+++ apps/tedrisat/src/app.module.ts
@@ -4,6 +4,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { configuration } from './config';
+import { ExampleModule } from './example/example.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -13,6 +15,8 @@ import { configuration } from './config';
       envFilePath: '.env',
     }),
     LoggerModule.forRoot(),
+    DatabaseModule,
+    ExampleModule,
   ],
   controllers: [AppController],
   providers: [AppService],
