27d44c2979f6559e649a1b91e202ac6afa73b880
zahidcakici <cakici19@itu.edu.tr>
2025-08-21 18:21:15 +0300

    feat: auto migrate at runtime

--- apps/tedrisat/src/database/database.service.ts
+++ apps/tedrisat/src/database/database.service.ts
@@ -1,15 +1,27 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import {
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+  Inject,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
+import { migrate } from 'drizzle-orm/node-postgres/migrator';
 import { Pool } from 'pg';
 import * as schema from './schema';
+import { ILogger, LOGGER } from '@madrasah/common';
 
 @Injectable()
 export class DatabaseService implements OnModuleInit, OnModuleDestroy {
   private pool!: Pool;
   public db!: ReturnType<typeof drizzle>;
 
-  constructor(private configService: ConfigService) {}
+  constructor(
+    private configService: ConfigService,
+    @Inject(LOGGER) private readonly logger: ILogger,
+  ) {
+    this.logger.setContext(DatabaseService.name);
+  }
 
   async onModuleInit() {
     this.pool = new Pool({
@@ -23,12 +35,20 @@ export class DatabaseService implements OnModuleInit, OnModuleDestroy {
 
     this.db = drizzle(this.pool, { schema });
 
+    await migrate(this.db, { migrationsFolder: './src/database/migrations' })
+      .then(() => {
+        this.logger.log('Migrations completed successfully');
+      })
+      .catch(() => {
+        this.logger.error('Migrations failed');
+      });
+
     // Test connection
     try {
       await this.pool.query('SELECT 1');
-      console.log('Database connected successfully');
+      this.logger.log('Database connected successfully');
     } catch (error) {
-      console.error('Database connection failed:', error);
+      this.logger.error('Database connection failed:');
       throw error;
     }
   }
