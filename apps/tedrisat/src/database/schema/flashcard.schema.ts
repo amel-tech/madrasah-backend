37538dfe8439434368b28c6ea5c8e86cebf42276
timurci <timurcakmakoglu@gmail.com>
2025-08-27 20:56:29 +0300

    feat: added drizzle schemas for the flashcard module

---
+++ apps/tedrisat/src/database/schema/flashcard.schema.ts
@@ -0,0 +1,32 @@
+import {
+  pgTable as table,
+  integer,
+  text,
+  timestamp,
+  pgEnum,
+  jsonb,
+} from 'drizzle-orm/pg-core';
+import { InferInsertModel, InferSelectModel } from 'drizzle-orm';
+import { flashcardDecks } from './flashcard-deck.schema';
+
+export const flashcardTypeEnum = pgEnum('flashcard_type', [
+  'VOCABULARY',
+  'HADEETH',
+]);
+
+export const flashcards = table('flashcards', {
+  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+  type: flashcardTypeEnum().notNull(),
+  authorId: integer('author_id').notNull(),
+  deckId: integer('deck_id')
+    .references(() => flashcardDecks.id, { onDelete: 'cascade' })
+    .notNull(),
+  contentFront: text('content_front').notNull(),
+  contentBack: text('content_back').notNull(),
+  contentMeta: jsonb('content_meta'),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
+
+export type Flashcard = InferSelectModel<typeof flashcards>;
+export type NewFlashcard = InferInsertModel<typeof flashcards>;
